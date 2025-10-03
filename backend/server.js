65eedf4c8e66d9b8933fdec0ee801dda2738d600
pikacoder44 <pikaop125@gmail.com>
2025-10-03 10:51:51 +0000

    Gave structure for backend

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,23 @@
 // Build a Book Review Platform where users can sign up, log in, add books, and review books. The goal is to test your skills in MongoDB, Express, React, Node.js (MERN) along with authentication, CRUD operations, and frontend integration.
 
 const express = require("express");
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const connectDB = require("./config/db");
 
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const MONGO_URI =
-  process.env.MONGO_URI || "mongodb://localhost:27017/bookreviewplatform";
 
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
 // Connect to MongoDB
-mongoose
-  .connect(MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
